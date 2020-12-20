f86481064164c51984bf511e90dc2cf509234e2c
sanighen <684257913>
2020-12-20 16:29:24 +0200

    client-server interaction

--- src/main/java/client/ClientChatApplication.java
+++ src/main/java/client/ClientChatApplication.java
@@ -14,25 +14,21 @@ public class ClientChatApplication {
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		Connection con = new Connection("localhost", ServerChatApplication.PORT);
 
-//		Scanner in = new Scanner(System.in);
-		
-//		User user = new User(in.next());
-		
-		User user = new User("Kolea");
-		
+		Scanner in = new Scanner(System.in);
+
+		User user = new User(in.next());
+
 		con.send(new Action(Operation.SIGN_IN, user));
-		
-//		System.out.println("CLIENT >>> server sent: " + con.fetch());
-		
+
 		Action action = (Action) con.fetch();
-		
+
 		if (action.getType().equals(Operation.SUCCESS))
 			System.out.println("CLIENT >>> server sent: Signed in");
-		
+
 		con.send(new Action(Operation.USER_LIST));
-		
+
 		System.out.println(action.getType());
-		
+
 	}
 
 }
